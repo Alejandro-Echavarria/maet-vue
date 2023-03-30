1a4e0ccc8841c342cfc7fbed45928e2a4c76e060
Manuel Echavarria <mechavarria@aldereca.com>
2023-03-30 16:16:20 -0400

    Primer commit

---
+++ src/components/CounterButton.vue
@@ -0,0 +1,34 @@
+<template>
+    <h1>Hola desde el componente nuevo</h1>
+    <button @click="decrement()">
+        -
+    </button>
+    {{ counter }}
+    <button @click="increment()">
+        +
+    </button>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+        counter: 0
+        }
+    },
+    methods: {
+        increment() {
+            this.counter++;
+        },
+        decrement() {
+            this.counter--;
+        }
+    }
+}
+</script>
+
+<style scoped>
+    h1{
+        color: red;
+    }
+</style>
